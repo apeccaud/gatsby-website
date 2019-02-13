80ad2a88b1dab5e984a62b09c72ed7f03cd5a7e8
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2019-02-13 22:31:26 +0800

    Header : add overlay on scroll down

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,67 +1,108 @@
-import React from "react";
+import React, { Component } from "react";
 import { css } from "@emotion/core";
 import { Link, graphql, StaticQuery } from "gatsby";
 
-export default ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div>
+export default class AboutModal extends Component {
+  state = {
+    headerOverlay: false
+  }
 
-        <header
-          css={css`
-            position: fixed;
-            top: 0; left: 0; right: 0;
-            background-color: transparent;
-            border-bottom: 1px solid #f1f1f1;
-            padding: 16px 60px;
-          `}
-        >
-          
-          <Link to={`/`}>
-            <h3
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (e) => {
+    // Round offset in order to improve performances
+    let offsetRounded = Math.ceil(window.pageYOffset / 63) * 63
+    
+    if (offsetRounded >= 63) {
+      this.setState({headerOverlay: true})
+    }
+    else {
+      this.setState({headerOverlay: false})
+    }
+  }
+
+  render () {
+    return (
+      <StaticQuery
+
+        query={graphql`
+          query {
+            site {
+              siteMetadata {
+                title
+              }
+            }
+          }
+        `}
+
+        render={data => (
+          <div>
+
+            <header
               css={css`
-                display: inline-block;
-                margin: 0;
+                position: fixed;
+                top: 0; left: 0; right: 0;
+                border-bottom: 1px solid #f1f1f1;
+                padding: 0 60px;
+                height: 60px;
+                line-height: 60px;
+                z-index: 999;
               `}
             >
-              {data.site.siteMetadata.title}
-            </h3>
-          </Link>
+              
+              <Link to={`/`}>
+                <h3
+                  css={css`
+                    display: inline-block;
+                    margin: 0;
+                  `}
+                >
+                  {data.site.siteMetadata.title}
+                </h3>
+              </Link>
 
-          <Link to={`/about/`}
-            css={css`
-              float: right;
-            `}
-          >
-            About
-          </Link>
+              {/* <Link to={`/about/`}
+                css={css`
+                  float: right;
+                `}
+              >
+                About
+              </Link> */}
 
-        </header>
+            </header>
+
+            
+            <div
+              css={css`
+                position: fixed;
+                top: 0; left: 0; right: 0;
+                background-color: #fff;
+                height: 60px;
+                transition: opacity 0.5s;
+                opacity: ${this.state.headerOverlay ? 1 : 0};
+              `}>
+              {/* Header Overlay */}
+            </div>
+            
+
+            <div>
+              {this.props.children}
+            </div>
 
 
-        <div
-          css={css`
-            margin-top: 63px;
-          `}
-        >
-          {children}
-        </div>
+            <footer>
+              Footer
+            </footer>
 
-
-        <footer>
-          Footer
-        </footer>
-
-      </div>
-    )}
-  />
-);
+          </div>
+        )}
+      />
+    )
+  }
+};
